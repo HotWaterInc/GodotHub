9d2f9dd3306f94677eaf7ab292c844801aa73f95
arking115 <rusalexandru115@gmail.com>
2024-04-15 10:12:14 +0300

    module setup

---
+++ HotWaterCLI /main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  HotWaterCLI
+//
+//  Created by Rus Alexandru on 11.04.2024.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include "json.hpp"
+#include <fstream>
+
+using namespace std;
+using json = nlohmann::json;
+
+int main(int argc, const char * argv[]) {
+    json j;
+    j["pi"] = 3.14;
+    string filePath = "modules.json";
+    
+    ofstream outputFile(filePath);
+    if (!outputFile.is_open()){
+        cerr << "Failed to open" << filePath << endl;
+        return  1;
+    }
+
+    // Print the JSON object
+    outputFile << j.dump(4);
+    // To print in a pretty format with indentation, you can do:
+    outputFile.close();
+    
+    cout << "JSON data has been written to " << filePath << endl;
+
+    return 0;
+}
