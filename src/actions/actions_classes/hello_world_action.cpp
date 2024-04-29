ab74ca1cf27c5a1eb7c672cc9379cdfcdfc6b986
erupturatis <barbulescu.eugeno@gmail.com>
2024-04-29 17:51:46 +0300

    module compilation done

--- src/actions/actions_classes/hello_world_action.cpp
+++ src/actions/actions_classes/hello_world_action.cpp
@@ -3,25 +3,96 @@
 #include "actions/action_abstract.h"
 #include "cmd/IO_singleton.h"
 #include "tasks/tasks.h"
-
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
 
 HelloWorldAction::HelloWorldAction() {
-	std::cout << "HelloWorldAction constructor" << std::endl;
+	// std::cout << "HelloWorldAction constructor" << std::endl;
 }
 
 HelloWorldAction::~HelloWorldAction() = default;
 
+void request_response_example()
+{
+	RequestResponseCallback request_response = SingletonIO::getInstance().get_request_response_callback();
+	std::vector<std::string> answers = {"yes", "no"};
+	std::string response = (*request_response)("Answer question with one of the following:", answers, answers[0]);
+}
 
 void HelloWorldAction::execute() {
-	RequestResponseCallback request_response = SingletonIO::getInstance().get_request_response_callback();
+	std::string base_path = "/home/eugen/godot_projects";
+	std::string project_name = "project1";
+	std::string mod_name = "mod1";
 
-//	std::vector<std::string> answers = {"yes", "no"};
-//	std::string response = (*request_response)("Answer question with one of the following:", answers, answers[0]);
-	
-	int godot_present = task_check_in_path("godot");
-	std::string base_path_godot = "~/godot";
-	std::string base_path_project = "~/godot/projects/project1";
-	
+	// builds up necessary templates data, Sconstruct and gd extension
+
+	// replaces {MOD_NAME} with mod_name
+	// replaces {PROJECT_NAME} with project_name
+	// places SConstruct in the base_path directory
+	// places the mod_name.gd in the base_path/project_name/bin directory
+
+	std::cout<<"Hello World Action executed"<<std::endl;
+
+	// They are build adjacent to the executable
+    std::string sconstruct_template_path = "./SConstruct_template";
+    std::string gdextension_template_path = "./mod_template.gdextension";
+
+    // Output file paths
+    std::string sconstruct_output_path = base_path + "/SConstruct";
+    std::string gdextension_output_path = base_path + "/" + project_name + "/bin/" + mod_name + ".gdextension";
+
+    // Read and replace {PROJECT_NAME} in SConstruct template
+    std::ifstream sconstruct_file(sconstruct_template_path);
+    std::stringstream sconstruct_buffer;
+    sconstruct_buffer << sconstruct_file.rdbuf();
+    sconstruct_file.close();
+
+    std::string sconstruct_content = sconstruct_buffer.str();
+    size_t project_name_pos = sconstruct_content.find("{PROJECT_NAME}");
+    while (project_name_pos != std::string::npos) {
+    	std::cout<<"Replacing project name"<<std::endl;
+        sconstruct_content.replace(project_name_pos, std::string("{PROJECT_NAME}").length(), project_name);
+        project_name_pos = sconstruct_content.find("{PROJECT_NAME}", project_name_pos + project_name.length());
+    }
+    size_t mod_name_pos = sconstruct_content.find("{MOD_NAME}");
+    while (mod_name_pos != std::string::npos) {
+    	std::cout<<"Replacing mod name"<<std::endl;
+        sconstruct_content.replace(mod_name_pos, std::string("{MOD_NAME}").length(), mod_name);
+        mod_name_pos = sconstruct_content.find("{MOD_NAME}", mod_name_pos + mod_name.length());
+    }
+	std::cout<<"Writing to SConstruct"<<std::endl;
+
+	std::cout<<sconstruct_output_path<<std::endl;
+
+    std::ofstream sconstruct_outfile(sconstruct_output_path);
+    sconstruct_outfile << sconstruct_content;
+    sconstruct_outfile.close();
+
+    // Read and replace {MOD_NAME} in GDExtension template
+    std::ifstream gdextension_file(gdextension_template_path);
+    std::stringstream gdextension_buffer;
+    gdextension_buffer << gdextension_file.rdbuf();
+    gdextension_file.close();
+
+    std::string gdextension_content = gdextension_buffer.str();
+
+    mod_name_pos = gdextension_content.find("{MOD_NAME}");
+    while (mod_name_pos != std::string::npos) {
+		gdextension_content.replace(mod_name_pos, std::string("{MOD_NAME}").length(), mod_name);
+		mod_name_pos = gdextension_content.find("{MOD_NAME}", mod_name_pos + mod_name.length());
+    }
+
+    std::ofstream gdextension_outfile(gdextension_output_path);
+    gdextension_outfile << gdextension_content;
+    gdextension_outfile.close();
+
+	// running final command
+	std::string command = "cd " + base_path + " && scons platform=linux";
+	std::cout<<"Running command: "<<command<<std::endl;
+	system(command.c_str());
+	std::cout<<"Command executed"<<std::endl;
 }
 
 
